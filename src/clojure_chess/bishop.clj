be0e40e53bf6e419f7fda0e280759372fb1cf1b8
Ivan Petrovic <ivan42382@gmail.com>
2016-08-24 16:40:27 +0200

    Functions for calculating bishop moves from xy field added.

---
+++ src/clojure_chess/bishop.clj
@@ -0,0 +1,61 @@
+(ns clojure-chess.bishop
+  (:require [clojure.repl :refer :all]
+            [clojure.core :refer :all]
+            [clojure-chess.rook :refer :all]
+            [clojure-chess.chessman :refer :all]
+            [clojure-chess.chessboard :refer :all]))
+
+(defn- get-bishop-possible-up-left-moves
+  "Returns list of maps that represents fields from excluded xy to the end of the board over up-left diagonal. 
+   If passed in values are not valid, nil is returned. "
+  [x y]
+  (if (valid-xy? x y)
+    (let [x-values (reverse (take (x-as-num x) x-value))
+          x-count (count x-values)]
+    (map #(hash-map %1 %2) x-values (take x-count (range (inc y) 8))))))
+
+(defn- get-bishop-possible-up-right-moves
+  "Returns list of maps that represents fields from excluded xy to the end of the board over up-right diagonal. 
+   If passed in values are not valid, nil is returned. "
+  [x y]
+  (if (valid-xy? x y)
+    (let [x-values (drop (inc (x-as-num x)) x-value)
+          x-count (count x-values)]
+    (map #(hash-map %1 %2) x-values (take x-count (range (inc y) 8))))))
+
+(defn- get-bishop-possible-down-left-moves
+  "Returns list of maps that represents fields from excluded xy to the end of the board over down-left diagonal. 
+   If passed in values are not valid, nil is returned. "
+  [x y]
+  (if (valid-xy? x y)
+    (let [x-values (reverse (take (x-as-num x) x-value))
+          x-count (count x-values)]
+    (map #(hash-map %1 %2) x-values (take x-count (reverse (range 0 y)))))))
+
+(defn- get-bishop-possible-down-right-moves
+  "Returns list of maps that represents fields from excluded xy to the end of the board over down-right diagonal. 
+   If passed in values are not valid, nil is returned. "
+  [x y]
+  (if (valid-xy? x y)
+    (let [x-values (drop (inc (x-as-num x)) x-value)
+          x-count (count x-values)]
+    (map #(hash-map %1 %2) x-values (take x-count (reverse (range 0 y)))))))
+
+(defn bishop-moves-vectors
+  "Returns vector containing 4 vectors (for each diagonal direction) that represents all possible moves 
+   on the empty board from possition xy. If possiton xy is not valid, nil is returned. "
+  [x y]
+  (if (valid-xy? x y)
+    (reduce #(conj %1 (%2 x y)) [] [get-bishop-possible-up-left-moves get-bishop-possible-up-right-moves 
+                                    get-bishop-possible-down-left-moves get-bishop-possible-down-right-moves])))
+
+(defn get-bishop-possible-moves
+  "Returns map {:eat {...} :move {...}} with all possible moves for the bishop located on xy field. Moves are separated in two maps: 
+   1. :eat - Where bishop can eat opponent chassman
+   2. :move - Where bishop can be placed (fields that are not occupied). 
+   If xy is not valid possition or xy is not occupied by bishop, nil is returned. "
+  [chessboard x y]
+  (if (and (valid-xy? x y) 
+           (occupied? chessboard x y)
+           (is-chessman-type? chessboard x y \b))
+    (get-chessman-possible-moves chessboard x y bishop-moves-vectors)))
--- src/clojure_chess/rook.clj
+++ src/clojure_chess/rook.clj
@@ -4,35 +4,35 @@
             [clojure-chess.chessman :refer :all]
             [clojure-chess.chessboard :refer :all]))
 
-(defn- get-rook-possible-up-moves
+(defn get-rook-possible-up-moves
   "Returns list of maps that represents fields from excluded xy to the top edge of the board. 
    If passed in values are not valid, nil is returned. "
   [x y]
   (if (valid-xy? x y)
     (map #(hash-map x %) (range (inc y) 8))))
 
-(defn- get-rook-possible-down-moves
+(defn get-rook-possible-down-moves
   "Returns list of maps that represents fields from excluded xy to the bottom edge of the board. 
    If passed in values are not valid, nil is returned. "
   [x y]
   (if (valid-xy? x y)
     (map #(hash-map x %) (reverse (range 0 y)))))
 
-(defn- get-rook-possible-right-moves
+(defn get-rook-possible-right-moves
   "Returns list of maps that represents fields from excluded xy to the right edge of the board. 
    If passed in values are not valid, nil is returned. "
   [x y]
   (if (valid-xy? x y)
     (map #(hash-map (x-as-key %) y) (range (if (nil? (inc-x x)) 8 (x-as-num (inc-x x))) 8))))
 
-(defn- get-rook-possible-left-moves
+(defn get-rook-possible-left-moves
   "Returns list of maps that represents fields from excluded xy to the left edge of the board. 
    If passed in values are not valid, nil is returned. "
   [x y]
   (if (valid-xy? x y)
     (map #(hash-map (x-as-key %) y) (reverse (range 0 (x-as-num x))))))
 
-(defn- rook-moves-vectors
+(defn rook-moves-vectors
   "Returns vector containing 4 vectors (for each direction) that represents all possible moves 
    on the empty board from possition xy. If possiton xy is not valid, nil is returned. "
   [x y]
@@ -41,9 +41,9 @@
                                     get-rook-possible-right-moves get-rook-possible-left-moves])))
 
 (defn get-rook-possible-moves
-  "Returns map {:eat {...} :move {...}} with all possible moves of the rook located on xy field. Moves are separated in two maps: 
+  "Returns map {:eat {...} :move {...}} with all possible moves for the rook located on xy field. Moves are separated in two maps: 
    1. :eat - Where rook can eat opponent chassman
-   2. :move - Where rook can be placed (fields that are not occupied. ). 
+   2. :move - Where rook can be placed (fields that are not occupied). 
    If xy is not valid possition or xy is not occupied by rook, nil is returned. "
   [chessboard x y]
   (if (and (valid-xy? x y) 
